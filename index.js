02aff62fafa76c2e50c49d41017bed29cfee1fd1
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2016-06-10 19:14:04 +0200

    mock test

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
 'use strict'
 
-var express = require('express');
-var app = express();
-var passport = require('passport');
-var pkg = require('./package.json');
-var bodyParser = require('body-parser');
-var config = require('config');
+var server = require('./src/server.js');
 
-var strategies = require('./src/auth/strategies');
-var googleOAuth = require('./src/auth/google');
-var routing = require('./src/routing');
-
-
-if( !process.env.NODE_ENV ){
-  process.env.NODE_ENV = 'development';
-}
-
-var db = require('./src/db');
-db.connect();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(passport.initialize())
-
-googleOAuth(app);
-
-routing(app);
-
-var env = config.get('env');
-var dbconf = config.get('database');
-
-app.listen( process.env.PORT || env.port, function() {
-  process.title = pkg.name
-  console.log(`${pkg.name}-v${pkg.version} is listening on port ${env.port}!
-    - CONFIG  : ./config/${process.env.NODE_ENV}.json
-    - DBURL   : ${dbconf.url}
-    - PID     : ${process.pid}
-    `);
-});
+server.start();
