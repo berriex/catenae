4ee69f88f24200c28ba84dd0ba83e7a00f87c96f
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2016-06-28 09:36:31 +0200

    feat(system): add bluebird as mongoose.promise

--- src/db.js
+++ src/db.js
@@ -2,12 +2,14 @@
 
 var mongoose = require('mongoose');
 var config = require('config');
+var Promise = require('bluebird');
 
 var params = config.get('database');
 
 var db = {
 
   connect(){
+    mongoose.Promise = Promise;
     if ( mongoose.connection.readyState < 1 ) {
       this._conn = mongoose.connect(params.url);
     }
